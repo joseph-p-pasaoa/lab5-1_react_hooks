c2624b24d3f31ccffa0b82738a1824029ceb49d2
joseph-p-pasaoa <joseph.p.pasaoa@gmail.com>
2020-01-28 20:53:03 -0500

    0.3, convert lab4-4_react_props to use hooks

--- hooked_lab4-4_react_props/src/App.js
+++ hooked_lab4-4_react_props/src/App.js
@@ -5,7 +5,7 @@ APP | Go Fund Me Lab
 
 
 /* IMPORTS */
-import React, { Component } from 'react';
+import React, { useState } from 'react';
 import './App.css';
 
 import TopBar from './components/TopBar';
@@ -15,91 +15,92 @@ import DonationForm from './components/DonationForm';
 
 
 /* MAIN */
-class App extends Component {
-  constructor() {
-    super();
-    this.target = 1000;
-    this.state = {
-      donations: [
+const App = () => {
+
+  // SET DATA
+    const target = 1000;
+
+  // USESTATE HOOKS
+    const [ donations, setDonations ] = useState([
         { name: "Joey", amount: 211, msg: "Good luck, Alejo! May your trip be all you imagined and more!" },
         { name: "JR", amount: 400, msg: "wish I were the one going!!!" },
         { name: "Wynter", amount: 50, msg: "Bon voyage!" },
         { name: "Dessa", amount: 100, msg: "Bring me back something cool~" }
-      ],
-      nameValue: "",
-      amountValue: 0,
-      msgValue: "",
-      errorMsgOpacity: { opacity: 0 }
+    ]);
+    const [ nameValue, setNameValue ] = useState("");
+    const [ amountValue, setAmountValue ] = useState(0);
+    const [ msgValue, setMsgValue ] = useState("");
+    const [ errorMsgOpacity, setErrorMsgOpacity ] = useState({ opacity: 0 });
+
+
+  // HANDLERS
+  const handleChange = (e) => {
+    switch (e.target.name) {
+      case "amountValue":
+        setAmountValue(Number(e.target.value));
+        break;
+      case "nameValue":
+        setNameValue(e.target.value);
+        break;
+      case "msgValue":
+        setMsgValue(e.target.value);
+        break;
+      default:
+        break;
     }
   }
 
-  calcRaised = (donationsArr) => {
-    return donationsArr.reduce((sum, curr) => sum += curr.amount, 0);
-  }
-
-  checkFormSubmit = () => {
-
-  }
-
-  handleSubmit = (e) => {
+  const handleSubmit = (e) => {
     e.preventDefault();
-    if (!this.state.amountValue) {
-      this.setState({
-          errorMsgOpacity: { opacity: 1 }
-      });
+    if (!amountValue) {
+      setErrorMsgOpacity({ opacity: 1 });
     } else {
       const newDonation = {
-        name: this.state.nameValue,
-        amount: this.state.amountValue,
-        msg: this.state.msgValue
+        name: nameValue,
+        amount: amountValue,
+        msg: msgValue
       }
-      const updatedDonations = this.state.donations.concat(newDonation);
+      const updatedDonations = [ newDonation, ...donations ];
 
-      this.setState({
-          donations: updatedDonations,
-          nameValue: "",
-          amountValue: 0,
-          msgValue: "",
-          errorMsgOpacity: { opacity: 0 }
-      });
+      setDonations(updatedDonations);
+      setNameValue("");
+      setAmountValue(0);
+      setMsgValue("");
+      setErrorMsgOpacity({ opacity: 0 });
     }
   }
 
-  handleChange = (e) => {
-    this.setState({
-        [e.target.name]: e.target.name === 'amountValue' ? Number(e.target.value) : e.target.value
-    });
-  }
 
-  render() {
-    const { donations, nameValue, amountValue, msgValue, errorMsgOpacity } = this.state;
-    const raised = this.calcRaised(donations);
-    const percentToTarget = (raised / this.target * 100).toFixed(2);
-    return (
-      <div className="App">
-        <div id="grid-base">
-          <TopBar />
-          <RecentDonations 
-            donations={donations}
-          />
-          <Progress 
-            target={this.target} 
-            raised={raised}
-            percentToTarget={percentToTarget}
-          />
-          <DonationForm 
-            handleSubmit={this.handleSubmit} 
-            handleChange={this.handleChange} 
-            nameValue={nameValue}
-            amountValue={amountValue}
-            msgValue={msgValue}
-            errorMsgOpacity={errorMsgOpacity} 
-          />
-          <footer>Copyright ©2019 Joseph P. Pasaoa. All rights reserved.</footer>
-        </div>
+  // PRE-RETURN
+  const raised = donations.reduce((sum, curr) => sum += curr.amount, 0);
+  const percentToTarget = (raised / target * 100).toFixed(2);
+
+
+  // RETURN
+  return (
+    <div className="App">
+      <div id="grid-base">
+        <TopBar />
+        <RecentDonations 
+          donations={donations}
+        />
+        <Progress 
+          target={target} 
+          raised={raised}
+          percentToTarget={percentToTarget}
+        />
+        <DonationForm 
+          handleSubmit={handleSubmit} 
+          handleChange={handleChange} 
+          nameValue={nameValue}
+          amountValue={amountValue}
+          msgValue={msgValue}
+          errorMsgOpacity={errorMsgOpacity} 
+        />
+        <footer>Copyright ©2019 Joseph P. Pasaoa. All rights reserved.</footer>
       </div>
-    );
-  }
+    </div>
+  );
 }
 
 
